380dbfbea718224782b342a232b3782db191e541
Scheker Badjaj <schebad@mail.de>
2025-11-10 19:56:20 +0100

    feat(loading): add animated GIF for loading visual

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderPrimaryComponent } from './layout/headers/header-primary/header-primary.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderPrimaryComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
