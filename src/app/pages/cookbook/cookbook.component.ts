6cc8e68688a44972cc48d47b88ed376031ffc4ae
Scheker Badjaj <schebad@mail.de>
2025-11-14 07:10:11 +0100

    feat(cookbook): fix layout overflow and add goBack navigation

--- src/app/pages/cookbook/cookbook.component.ts
+++ src/app/pages/cookbook/cookbook.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { HeaderSecondaryComponent } from '../../layout/headers/header-secondary/header-secondary.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cookbook',
   standalone: true,
-  imports: [],
+  imports: [HeaderSecondaryComponent, RouterLink],
   templateUrl: './cookbook.component.html',
-  styleUrl: './cookbook.component.scss'
+  styleUrls: ['./cookbook.component.scss'],
 })
 export class CookbookComponent {
+  constructor(private location: Location) { }
 
+  goBack(): void {
+    this.location.back();
+  }
 }
